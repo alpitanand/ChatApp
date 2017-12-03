0fd41f5c25834f3398c2dcdbffc32b3533199c6b
alpitanand <alpitanand20@gmail.com>
2017-12-03 12:15:21 +0530

    Starting chatApp

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+    console.log(`Server is up on 3000 ${port}`);
+})
