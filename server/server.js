b19c8859beb6a46a67dc1afdad754e2776d65142
alpitanand <alpitanand20@gmail.com>
2017-12-13 20:37:15 +0530

    Commited

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
-
+const socketIO = require('socket.io');
+const http = require('http');
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(publicPath));
+io.on('connection', (socket) => {
+    console.log('New user connected from server');
+    socket.on('disconnect', () => {
+        console.log('Disconneccted from server')
+    });
+})
 
-app.listen(3000, () => {
-    console.log(`Server is up on 3000 ${port}`);
+server.listen(3000, () => {
+    console.log(`Server is up on ${port}`);
 })
