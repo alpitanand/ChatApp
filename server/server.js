b3fafbfff631d24cfc56257c3106ffdee79540f2
alpitanand <alpitanand20@gmail.com>
2017-12-21 18:19:23 +0530

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -11,15 +11,19 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected from server');
+    socket.emit('newMessage',{
+        from:'Admin',
+        text:'Welcome to the chatApp'
+    })
+    socket.broadcast.emit('newMessage',{
+            from:'Admin',
+            text:'New user joined',
+            createdAt:Math.floor(Date.now() / 1000)
+        })
     socket.on('disconnect', () => {
         console.log('Disconneccted from server')
     });
-    socket.emit('newEmail', {
-        from: "alpitanand20@gmail.com",
-        text: "Hey prachee i will be back home soon",
-        number: 9778260806,
-        createdAt: Math.floor(Date.now() / 1000)
-    });
+   
      
     socket.on('createEmail', (newEmail) => {
         console.log(newEmail);
@@ -31,6 +35,11 @@ io.on('connection', (socket) => {
             text:message.text,
             createdAt:Math.floor(Date.now() / 1000)
         })
+        // socket.broadcast.emit('newMessage',{
+        //     from:message.from,
+        //     text:message.text,
+        //     createdAt:Math.floor(Date.now() / 1000)
+        // })
     });
 })
 
