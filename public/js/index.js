0032bf01bc3ceadb72b29cb52b56062a05bec7a7
alpitanand <alpitanand20@gmail.com>
2017-12-24 21:47:40 +0530

    Added UI function

--- public/js/index.js
+++ public/js/index.js
@@ -9,8 +9,26 @@ socket.on('disconnect', function () {
 
 socket.on('newEmail', function (email) {
     console.log(email);
+    
 })
 
 socket.on('newMessage',function(message){
     console.log(message);
+    var html = '<li>%item%</li>'
+    var newHtml = html.replace('%item%', `${message.from}: ${message.text}`)
+    
+    document.getElementById('text_messages').insertAdjacentHTML('beforeend',newHtml);
 })
+
+
+document.getElementById('message-form').addEventListener('submit',function(e){
+e.preventDefault();
+
+socket.emit('createMessage',{
+    from:'User',
+    text : document.querySelector('.message').value
+    },function(data){
+        console.log(data);
+    })
+    
+});
